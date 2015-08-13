47990608c6fc5bbac9f2528cb0ea6feb87b084c4
anhnv <anhnv1310@gmail.com>
2015-08-13 20:18:15 +0700

    edit file..

--- EMRDEMO/EMRDEMO/FormMain/Form1.Designer.cs
+++ EMRDEMO/EMRDEMO/FormMain/Form1.Designer.cs
@@ -30,31 +30,55 @@ namespace FormMain
         /// </summary>
         private void InitializeComponent()
         {
-            this.editor1 = new Editor();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.editor1 = new ERMControl.Editor();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.editor1);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(1104, 475);
+            this.splitContainerControl1.SplitterPosition = 320;
+            this.splitContainerControl1.TabIndex = 0;
+            this.splitContainerControl1.Text = "splitContainerControl1";
             // 
             // editor1
             // 
-            this.editor1.Location = new System.Drawing.Point(360, 4);
+            this.editor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editor1.HashFileNameDict = ((System.Collections.Generic.Dictionary<string, string>)(resources.GetObject("editor1.HashFileNameDict")));
+            this.editor1.Location = new System.Drawing.Point(0, 0);
             this.editor1.Name = "editor1";
-            this.editor1.Size = new System.Drawing.Size(588, 465);
-            this.editor1.TabIndex = 0;
+            this.editor1.Size = new System.Drawing.Size(779, 475);
+            this.editor1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(949, 472);
-            this.Controls.Add(this.editor1);
+            this.ClientSize = new System.Drawing.Size(1104, 475);
+            this.Controls.Add(this.splitContainerControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
         private Editor editor1;
+
+
     }
 }
 
